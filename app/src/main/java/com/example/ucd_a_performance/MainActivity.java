c383a2ce6679240a6198b070ce3e5c5ef2372468
Mi6u3l <miguelbragagomes@gmail.com>
2019-08-01 20:16:14 +0100

    adds view button and action to get location

--- app/src/main/java/com/example/ucd_a_performance/MainActivity.java
+++ app/src/main/java/com/example/ucd_a_performance/MainActivity.java
@@ -3,13 +3,27 @@ package com.example.ucd_a_performance;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide(); // hide the title bar
+
+    }
+
+    public void startGPSTest(View view)
+    {
+        TextView feedback = findViewById(R.id.feedback);
+        GPSTracker gps = new GPSTracker(this);
+        double latitude = gps.getLatitude();
+        double longitude = gps.getLongitude();
+        feedback.setText("Latitude" + latitude + "Longitude" + longitude);
+        gps.showSettingsAlert();
     }
 }
