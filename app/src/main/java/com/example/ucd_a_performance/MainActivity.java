0fbfb406622699da9aa132410328f7861ba83699
Mi6u3l <miguelbragagomes@gmail.com>
2019-08-04 18:06:05 +0100

    removes timer cycle

--- app/src/main/java/com/example/ucd_a_performance/MainActivity.java
+++ app/src/main/java/com/example/ucd_a_performance/MainActivity.java
@@ -2,28 +2,49 @@ package com.example.ucd_a_performance;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.location.Location;
+
+import java.util.Date;
+import java.util.Calendar;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private FusedLocationProviderClient fusedLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().hide(); // hide the title bar
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
     }
 
-    public void startGPSTest(View view)
-    {
-        TextView feedback = findViewById(R.id.feedback);
-        GPSTracker gps = new GPSTracker(this);
-        double latitude = gps.getLatitude();
-        double longitude = gps.getLongitude();
-        feedback.setText("Latitude" + latitude + "Longitude" + longitude);
-        gps.showSettingsAlert();
+
+    @SuppressLint("MissingPermission")
+    public void startGPSTest(View view) {
+        final TextView feedback = findViewById(R.id.feedback);
+        final Date currentTime = Calendar.getInstance().getTime();
+
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            feedback.setText("Latitude " + location.getLatitude());// Logic to handle location object
+                        }
+                    }
+                });
     }
 }
